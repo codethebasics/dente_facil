ede5c64e8b312291620bb71b890415fa2b55b61f
codethebasics <codethebasics@gmail.com>
2024-03-29 13:53:29 -0300

    listagem de usuários

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,9 +3,29 @@
 import { useRouter } from 'next/navigation';
 import styles from './page.module.css';
 import Image from 'next/image';
+import { useState } from 'react';
+
+type LoginRequest = {
+  username?: string;
+  password?: string;
+};
 
 export default function LoginIndex() {
   const router = useRouter();
+
+  const [loginRequest, setLoginRequest] = useState<LoginRequest>({
+    username: '',
+    password: '',
+  });
+
+  const handleLogin = (event: any) => {
+    event.preventDefault();
+    console.log('login', loginRequest);
+    setLoginRequest({
+      username: '',
+      password: '',
+    });
+  };
 
   return (
     <div className={styles.container}>
@@ -17,14 +37,32 @@ export default function LoginIndex() {
           height={116}
         />
       </div>
-      <form className={styles.formCadastro}>
+      <form
+        className={styles.formCadastro}
+        onSubmit={handleLogin}
+        method="post"
+      >
         <div className={styles.control}>
           <label>E-mail</label>
-          <input type="email" placeholder="seu@email.com" />
+          <input
+            type="email"
+            placeholder="seu@email.com"
+            value={loginRequest?.username}
+            onChange={(evt) =>
+              setLoginRequest({ ...loginRequest, username: evt.target.value })
+            }
+          />
         </div>
         <div className={styles.control}>
           <label>Senha</label>
-          <input type="password" placeholder="*******" />
+          <input
+            type="password"
+            placeholder="*******"
+            value={loginRequest?.password}
+            onChange={(evt) =>
+              setLoginRequest({ ...loginRequest, password: evt.target.value })
+            }
+          />
         </div>
         <div className={styles.control}>
           <button>Entrar</button>
